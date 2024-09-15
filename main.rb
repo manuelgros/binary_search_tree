118ad959bde53a0e5255287ac4128ac221bfb983
manuelgros <131806742+manuelgros@users.noreply.github.com>
2024-09-15 15:36:34 -0700

    Formulated test cases

--- main.rb
+++ main.rb
@@ -5,11 +5,36 @@ require 'pry-byebug'
 
 #------------ Test Code ---------------
 
-arr = [1, 7, 4, 23, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# arr = [1, 7, 4, 23, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# tree = Tree.new(arr)
+# tree.pretty_print
+# tree.insert(2)
+# tree.pretty_print
+# tree.delete(67)
+# tree.pretty_print
+
+arr = Array.new(15) { rand(1..100) }
 tree = Tree.new(arr)
 tree.pretty_print
-tree.insert(2)
+puts ""
+p tree.balanced?
+puts ""
+p tree.preorder
+p tree.postorder
+p tree.inorder
+tree.insert(102)
+tree.insert(130)
+tree.insert(150)
+tree.insert(172)
 tree.pretty_print
-tree.delete(67)
+puts ""
+puts tree.balanced?
+puts ""
+tree.rebalance
 tree.pretty_print
-
+puts ""
+p tree.balanced?
+puts ""
+p tree.preorder
+p tree.postorder
+p tree.inorder
